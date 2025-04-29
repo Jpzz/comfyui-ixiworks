3298b47a86163b0363894bebc8cd50a06e13f639
Jpzz <36434952+Jpzz@users.noreply.github.com>
2025-04-29 15:13:27 +0900

    update

--- virtualinteraction_nodes.py
+++ virtualinteraction_nodes.py
@@ -15,7 +15,7 @@ class JsonParserNode:
     CATEGORY = "StoryBoard"
     
     def parse_text(self, file_name):
-        file_path = os.path.abspath(os.path.normpath(os.path.join(os.path.dirname(os.path.dirname(os.path.dirname(__file__))), "input", file_name)))
+        file_path = os.path.abspath(os.path.normpath(os.path.join(os.path.dirname(os.path.dirname(os.path.dirname(__file__))), "input", "prompt", file_name)))
         logging.info(f"[StoryBoard] JsonParserNode: file path '{file_path}'")
         try:
             # Load the JSON file
